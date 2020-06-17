957032b95c167e08146aef3d0eb0e1ec4bf89e63
knightvertrag <amiapu1@gmail.com>
2020-06-18 00:06:33 +0530

    Fixed deprecation warning

--- config/db.js
+++ config/db.js
@@ -8,6 +8,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useCreateIndex: true,
       useUnifiedTopology: true,
+      useFindAndModify: false,
     });
     console.log("MongoDB connected");
   } catch (err) {

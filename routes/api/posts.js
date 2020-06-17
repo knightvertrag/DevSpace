45000418c98ac8220086d624e3d4003827cb29b0
knightvertrag <amiapu1@gmail.com>
2020-06-18 01:09:06 +0530

    Built Post model

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
-// @route GET api/posts
-// @desc TEST route
-// @access Public
-router.get("/", (req, res) => res.send("Posts Route"));
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+// @route POST api/posts
+// @desc Create a post
+// @access Private
+router.post(
+  "/",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const user = await User.findById(req.user.id).select("-password");
+  }
+);
 
 module.exports = router;

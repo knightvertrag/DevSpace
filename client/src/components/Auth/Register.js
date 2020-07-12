8feef79f7dda271b49c9e2973c2b22e2f6130814
knightvertrag <amiapu1@gmail.com>
2020-07-13 03:35:26 +0530

    Redux Boilerplate

--- client/src/components/Auth/Register.js
+++ client/src/components/Auth/Register.js
@@ -16,7 +16,7 @@ const Register = () => {
 
 	const onSubmit = async (e) => {
 		e.preventDefault();
-		if (password != password2) {
+		if (password !== password2) {
 			console.log("Passwords do not match");
 		} else {
 			console.log("success");

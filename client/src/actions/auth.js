37b1bd1a3729f3d932e2a38fa21d89fc23b05f26
knightvertrag <amiapu1@gmail.com>
2020-07-13 19:39:59 +0530

    Set Up log in

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,6 +5,8 @@ import {
 	REGISTER_FAIL,
 	USER_LOADED,
 	AUTH_ERROR,
+	LOGIN_FAIL,
+	LOGIN_SUCCESS,
 } from "./types";
 import setAuthToken from "../utils/setAuthToken";
 // Load User
@@ -43,6 +45,8 @@ export const register = ({ name, email, password }) => async (dispatch) => {
 			type: REGISTER_SUCCESS,
 			payload: res.data,
 		});
+
+		dispatch(loadUser());
 	} catch (err) {
 		const errors = err.response.data.errors;
 
@@ -52,6 +56,38 @@ export const register = ({ name, email, password }) => async (dispatch) => {
 
 		dispatch({
 			type: REGISTER_FAIL,
+		});
+	}
+};
+
+// Login User
+export const login = (email, password) => async (dispatch) => {
+	const config = {
+		headers: {
+			"Content-Type": "application/json",
+		},
+	};
+
+	const body = JSON.stringify({ email, password });
+
+	try {
+		const res = await axios.post("/api/auth", body, config);
+
+		dispatch({
+			type: LOGIN_SUCCESS,
+			payload: res.data,
+		});
+
+		dispatch(loadUser());
+	} catch (err) {
+		const errors = err.response.data.errors;
+
+		if (errors) {
+			errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+		}
+
+		dispatch({
+			type: LOGIN_FAIL,
 		});
 	}
 };

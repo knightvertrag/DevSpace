073a1d9ae86fa697f03116543a419ea698833a9b
knightvertrag <amiapu1@gmail.com>
2020-07-13 20:01:43 +0530

    Completed Auth Functionality

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,6 +7,7 @@ import {
 	AUTH_ERROR,
 	LOGIN_FAIL,
 	LOGIN_SUCCESS,
+	LOGOUT,
 } from "./types";
 import setAuthToken from "../utils/setAuthToken";
 // Load User
@@ -90,4 +91,9 @@ export const login = (email, password) => async (dispatch) => {
 			type: LOGIN_FAIL,
 		});
 	}
+};
+
+//Logout / Clear Profile
+export const logout = () => (dispatch) => {
+	dispatch({ type: LOGOUT });
 };

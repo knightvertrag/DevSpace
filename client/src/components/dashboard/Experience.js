46fe4522271d0defe3540b79143d2eac607b91ac
knightvertrag <amiapu1@gmail.com>
2020-07-20 02:38:15 +0530

    Added date format logic

--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -10,7 +10,11 @@ const Experience = ({ experience }) => {
 			<td className="hide-sm">{exp.title}</td>
 			<td>
 				<Moment format="YYYY/MM/DD">{exp.from}</Moment> -{" "}
-				{exp.to === null ? "Now" : <Moment></Moment>}
+				{exp.to === null ? (
+					"Now"
+				) : (
+					<Moment format="YYYY/MM/DD">{exp.to}</Moment>
+				)}
 			</td>
 		</td>
 	));

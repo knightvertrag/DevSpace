7f1211088c2188fca6ec9893006957c241c7c3d1
knightvertrag <amiapu1@gmail.com>
2020-07-20 01:46:39 +0530

    Added State

--- client/src/components/profile-forms/AddExperience.js
+++ client/src/components/profile-forms/AddExperience.js
@@ -1,10 +1,27 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { Link, withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { addExperience } from "../../actions/profile";
 
 const AddExperience = (props) => {
+	const [formData, setFormData] = useState({
+		company: "",
+		title: "",
+		location: "",
+		from: "",
+		to: "",
+		current: false,
+		description: "",
+	});
+
+	const [toDateDisabled, toggleDisabled] = useState(false);
+
+	const { company, title, location, from, to, current, description } = formData;
+
+	const onChange = (e) =>
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+
 	return (
 		<Fragment>
 			<h1 class="large text-primary">Add An Experience</h1>

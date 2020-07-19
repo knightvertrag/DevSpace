96e2d373ab35f7e8ce5c1fcf357690c604698e50
knightvertrag <amiapu1@gmail.com>
2020-07-20 01:31:42 +0530

    Added education action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -89,3 +89,36 @@ export const addExperience = (formData, history) => async (dispatch) => {
 		});
 	}
 };
+
+// Add Education
+export const addEducation = (formData, history) => async (dispatch) => {
+	try {
+		const config = {
+			headers: {
+				"Content-Type": "application/json",
+			},
+		};
+
+		const res = await axios.put("/api/profile/education", formData, config);
+
+		dispatch({
+			type: UPDATE_PROFILE,
+			payload: res.data,
+		});
+
+		dispatch(setAlert("Education Added", "success"));
+
+		history.push("/dashboard");
+	} catch (err) {
+		const errors = err.response.data.errors;
+
+		if (errors) {
+			errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+		}
+
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status },
+		});
+	}
+};

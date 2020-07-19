fb76872b275ae1bdc325482ba9a754a6036bd84e
knightvertrag <amiapu1@gmail.com>
2020-07-20 01:27:23 +0530

    Created update action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -56,3 +56,38 @@ export const createProfile = (formData, history, edit = false) => async (
 		});
 	}
 };
+
+// Add Experience
+export const addExperience = (formData, history) => async (dispatch) => {
+	try {
+		const config = {
+			headers: {
+				"Content-Type": "application/json",
+			},
+		};
+
+		const res = await axios.put("/api/profile", formData, config);
+
+		dispatch({
+			type: GET_PROFILE,
+			payload: res.data,
+		});
+
+		dispatch(setAlert(edit ? "Profile Updated" : "Profile Created", "success"));
+
+		if (!edit) {
+			history.push("/dashboard");
+		}
+	} catch (err) {
+		const errors = err.response.data.errors;
+
+		if (errors) {
+			errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+		}
+
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status },
+		});
+	}
+};

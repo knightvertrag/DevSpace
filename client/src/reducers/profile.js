242359f26cf0c3371c85f7bfe67a3c6990219d3e
knightvertrag <amiapu1@gmail.com>
2020-07-22 01:38:09 +0530

    Added github repo reducer

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -4,6 +4,7 @@ import {
 	CLEAR_PROFILE,
 	UPDATE_PROFILE,
 	GET_PROFILES,
+	GET_REPOS,
 } from "../actions/types";
 
 const initialState = {
@@ -42,6 +43,12 @@ export default function (state = initialState, action) {
 				...state,
 				profile: null,
 				repos: [],
+				loading: false,
+			};
+		case GET_REPOS:
+			return {
+				...state,
+				repos: payload,
 				loading: false,
 			};
 		default:

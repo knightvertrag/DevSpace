7926e720445a453f6a92b5d76ec7049c5ea7417e
knightvertrag <amiapu1@gmail.com>
2020-07-22 02:00:02 +0530

    Added profile links to navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,6 +8,10 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 	const authLinks = (
 		<ul>
 			<li>
+				<Link to="/profiles">Developers</Link>
+			</li>
+			<li></li>
+			<li>
 				<Link to="/dashboard">
 					<i className="fas fa-user"></i>{" "}
 					<span className="hide-sm">Dashboard</span>
@@ -25,7 +29,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 	const guestLinks = (
 		<ul>
 			<li>
-				<Link to="#!">Developers</Link>
+				<Link to="/profiles">Developers</Link>
 			</li>
 			<li>
 				<Link to="/register">Register</Link>

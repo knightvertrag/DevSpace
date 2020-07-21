92b27665f563975be1cd56fbe281c97c35312e9b
knightvertrag <amiapu1@gmail.com>
2020-07-22 02:30:17 +0530

    Created Profile item for user

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -20,7 +20,17 @@ const ProfileItem = ({
 					{status} {company && <span> at {company}</span>}
 				</p>
 				<p className="my-1">{location && <span>{location}</span>}</p>
+				<Link to={`/profile/${_id}`} className="btn btn-primary">
+					View Profile
+				</Link>
 			</div>
+			<ul>
+				{skills.slice(0, 4).map((skill, index) => (
+					<li key={index} className="text-primary">
+						<i className="fas fa-check"></i> {skill}
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };

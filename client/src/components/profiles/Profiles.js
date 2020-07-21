3ab151f4834193dcde82cb78d04cf37fb271563c
knightvertrag <amiapu1@gmail.com>
2020-07-22 01:45:34 +0530

    Templated

---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,20 @@
+import React, { Fragment, useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Spinner from "../layout/Spinner";
+import { getProfiles } from "../../actions/profile";
+
+const Profiles = (props) => {
+	return <div></div>;
+};
+
+Profiles.propTypes = {
+	getProfiles: PropTypes.func.isRequired,
+	profile: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+	profile: state.profile,
+});
+
+export default connect(mapStateToProps, { getProfiles })(Profiles);

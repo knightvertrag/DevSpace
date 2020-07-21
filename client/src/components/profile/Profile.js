5255e06f917f9ef6e01d63b339bd9be6debbe902
knightvertrag <amiapu1@gmail.com>
2020-07-22 03:08:33 +0530

    Added a back to profiles page button

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect } from "react";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import { getProfileById } from "../../actions/profile";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 const Profile = ({
@@ -16,7 +17,15 @@ const Profile = ({
 
 	return (
 		<Fragment>
-			{profile === null || loading ? <Spinner /> : <Fragment>profile</Fragment>}
+			{profile === null || loading ? (
+				<Spinner />
+			) : (
+				<Fragment>
+					<Link to="/profiles" className="btn btn-light">
+						Back To Profiles
+					</Link>
+				</Fragment>
+			)}
 		</Fragment>
 	);
 };

912a96f65fe78459b85c547fa50235f81040589e
knightvertrag <amiapu1@gmail.com>
2020-07-22 01:31:13 +0530

    Added get all profiles reducer

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -3,6 +3,7 @@ import {
 	PROFILE_ERROR,
 	CLEAR_PROFILE,
 	UPDATE_PROFILE,
+	GET_PROFILES,
 } from "../actions/types";
 
 const initialState = {
@@ -24,7 +25,12 @@ export default function (state = initialState, action) {
 				profile: payload,
 				loading: false,
 			};
-
+		case GET_PROFILES:
+			return {
+				...state,
+				profiles: payload,
+				loading: false,
+			};
 		case PROFILE_ERROR:
 			return {
 				...state,
